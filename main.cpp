6cb2c6ed182a1063f3a10ea15ef329f069e53817
1evergreen <44565285+1evergreen@users.noreply.github.com>
2024-04-29 20:14:14 +0800

    implementation of the triangle and barycentric functino

--- main.cpp
+++ main.cpp
@@ -5,58 +5,113 @@
 #include "geometry.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
 Model *model = NULL;
-const int width  = 800;
+const int width = 800;
 const int height = 800;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
     bool steep = false;
-    if (std::abs(x0-x1)<std::abs(y0-y1)) {
+    if (std::abs(x0 - x1) < std::abs(y0 - y1))
+    {
         std::swap(x0, y0);
         std::swap(x1, y1);
         steep = true;
     }
-    if (x0>x1) {
+    if (x0 > x1)
+    {
         std::swap(x0, x1);
         std::swap(y0, y1);
     }
-
-    for (int x=x0; x<=x1; x++) {
-        float t = (x-x0)/(float)(x1-x0);
-        int y = y0*(1.-t) + y1*t;
-        if (steep) {
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int derror2 = std::abs(dy) * 2;
+    int error2 = 0;
+    int y = y0;
+    for (int x = x0; x <= x1; x++)
+    {
+        if (steep)
+        {
             image.set(y, x, color);
-        } else {
+        }
+        else
+        {
+            image.set(x, y, color);
+        }
+        error2 += derror2;
+        if (error2 > dx)
+        {
+            y += (y1 > y0 ? 1 : -1);
+            error2 -= dx * 2;
+        }
+    }
+}
+
+Vec2i world2screen(Vec3f v, TGAImage &image)
+{
+    int x = (v.x + 1) * image.get_width() / 2;
+    int y = (v.y + 1) * image.get_height() / 2;
+    return Vec2i{x, y};
+}
+
+Vec3f barycentric(Vec2i *pts, Vec2i P)
+{
+    Vec3f u = Vec3f(pts[2].x-pts[0].x, pts[1].x-pts[0].x, pts[0].x-P.x)^Vec3f(pts[2].y-pts[0].y, pts[1].y-pts[0].y, pts[0].y-P.y);
+    if(std::abs(u.z) < 1e-2 )
+        return Vec3f(-1, 1, 1);
+    return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+}
+void triangle(Vec2i *pts, TGAImage &image, TGAColor color)
+{
+    int xmin = std::min(std::min(pts[0].x, pts[1].x), pts[2].x);
+    int xmax = std::max(std::max(pts[0].x, pts[1].x), pts[2].x);
+    int ymin = std::min(std::min(pts[0].y, pts[1].y), pts[2].y);
+    int ymax = std::max(std::max(pts[0].y, pts[1].y), pts[2].y);
+
+    for (int x = xmin; x <= xmax; ++x)
+    {
+        for (int y = ymin; y <= ymax; ++y)
+        {
+            auto bary = barycentric(pts, Vec2i(x, y));
+            if(bary.x < 0 || bary.y < 0 || bary.z < 0)
+                continue;
             image.set(x, y, color);
         }
     }
 }
 
-int main(int argc, char** argv) {
-    if (2==argc) {
-        model = new Model(argv[1]);
-    } else {
-        model = new Model("obj/african_head.obj");
-    }
+int main(int argc, char **argv)
+{
+    // if (2 == argc)
+    // {
+    //     model = new Model(argv[1]);
+    // }
+    // else
+    // {
+    //     model = new Model("/Users/wangww/Desktop/tinyrenderer/obj/african_head.obj");
+    // }
 
-    TGAImage image(width, height, TGAImage::RGB);
-    for (int i=0; i<model->nfaces(); i++) {
-        std::vector<int> face = model->face(i);
-        for (int j=0; j<3; j++) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j+1)%3]);
-            int x0 = (v0.x+1.)*width/2.;
-            int y0 = (v0.y+1.)*height/2.;
-            int x1 = (v1.x+1.)*width/2.;
-            int y1 = (v1.y+1.)*height/2.;
-            line(x0, y0, x1, y1, image, white);
-        }
-    }
-
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("output.tga");
-    delete model;
+    // TGAImage image(width, height, TGAImage::RGB);
+    // Vec2i pts[3];
+    // std::vector<int> face;
+    // for (int i = 0; i < model->nfaces(); i++)
+    // {
+    //     face = model->face(i);
+    //     for (int j = 0; j < 3; j++)
+    //     {
+    //         pts[i] = world2screen(model->vert(face[j]), image);
+    //     }
+    //     triangle(pts, image, white);
+    // }
+    TGAImage frame(200, 200, TGAImage::RGB); 
+    Vec2i pts[3] = {Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160)}; 
+    triangle(pts, frame, red); 
+    frame.flip_vertically(); // to place the origin in the bottom left corner of the image 
+    frame.write_tga_file("/Users/wangww/Desktop/tinyrenderer/outputs/framebuffer.tga");
+    return 0; 
+    // image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    // image.write_tga_file("/Users/wangww/Desktop/tinyrenderer/outputs/output1.tga");
+    // delete model;
     return 0;
 }
-
